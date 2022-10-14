95c0916b274d7f9a0e04c06fce4e4d289fad51c3
jtnycu <jtnycu@gmail.com>
2022-10-14 19:25:46 +0000

    A1 sample code

---
+++ A1/A1_MNIST_CNN_classifier.py
@@ -0,0 +1,96 @@
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from sklearn.metrics import confusion_matrix
+
+
+torch.cuda.set_device(0)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device", torch.cuda.current_device(), torch.cuda.get_device_name(torch.cuda.current_device()))
+
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(0.5, 0.5)])
+
+trainset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
+
+
+class Net(nn.Module):
+    def __init__(self, classes=10):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 10 * 10, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+start_time = time.time()
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data[0].to(device), data[1].to(device)
+        optimizer.zero_grad()
+        loss = criterion(net(inputs), labels)
+        loss.backward()
+        optimizer.step()
+        if i == 0:
+            print(f"\nEpoch:{epoch+1}")
+        print(f"\r\tBatch:{i+1:03} of {len(trainloader)}, loss:{loss.item():.3f}", end="")
+print(f"\nFinished Training for {time.time()-start_time:.2f} s")
+
+class_correct = [0] * 10
+class_total = [0] * 10
+preds = np.zeros((1))
+truth = np.zeros((1))
+net.eval()
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images.to(device))
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted.cpu() == labels).squeeze()
+        preds = np.concatenate([preds, predicted.cpu().numpy()], axis=0)
+        truth = np.concatenate([truth, labels.numpy()], axis=0)
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+for i in range(10):
+    print("Accuracy of %5s : %2d %%" % (i, 100 * class_correct[i] / class_total[i]))
+print("\nAccuracy of   ALL : %2d %%" % (100 * sum(class_correct) / sum(class_total)))
+
+arr = confusion_matrix(truth, preds)
+class_names = list(range(10))
+df_cm = pd.DataFrame(arr, class_names, class_names)
+plt.figure(figsize = (9,6))
+sns.heatmap(df_cm, annot=True, fmt="d", cmap='BuGn')
+plt.xlabel("prediction")
+plt.ylabel("label (ground truth)")
